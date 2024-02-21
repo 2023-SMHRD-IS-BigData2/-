c9c8ee287a466dc79136c49f9135773fc9c8b163
정찬호 <hiierm@naver.com>
2024-02-21 14:44:25 +0900

    24.02.21 14:44 commit; 템플릿 내 html 연동 완료

--- CyberGuardians/src/main/java/com/cyberguardians/controller/QuestionController.java
+++ CyberGuardians/src/main/java/com/cyberguardians/controller/QuestionController.java
@@ -8,29 +8,44 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cyberguardians.entity.Javas_Question;
 import com.cyberguardians.repository.QuestionRepository;
 
 @Controller
-@RequestMapping("/question")
 public class QuestionController {
 
 	@Autowired
 	private QuestionRepository repo;
-
+	
+	@GetMapping("")
+    public String goMain() {
+        return "main";
+    }
+	
+	@GetMapping("/product")
+	public String toProduct() {
+		return "product";
+	}
+	
+	@GetMapping("/information")
+	public String toQuestion() {
+		return "information";
+	}
+	
 	@GetMapping("/list")
 	public String findAll(Model model) {
 		List<Javas_Question> questionList = repo.findAll();
 		System.out.println("questionList : " + questionList.get(0).getQuestion_index());
 		model.addAttribute("questionList", questionList);
-		return "question";
+		return "request";
 	}
 
 	@PostMapping("/write")
 	public String Write(Javas_Question question) {
 		repo.save(question);
-		return "redirect:/question/list";
+		return "redirect:/request/list";
 	}
-
+	
 }
